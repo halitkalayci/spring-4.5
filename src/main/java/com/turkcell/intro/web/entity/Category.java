e5d69f7e6af9e276412be097c41cb3543d804ce6
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-09-06 14:54:49 +0300

    .

---
+++ src/main/java/com/turkcell/intro/web/entity/Category.java
@@ -0,0 +1,30 @@
+package com.turkcell.intro.web.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="categories")
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
