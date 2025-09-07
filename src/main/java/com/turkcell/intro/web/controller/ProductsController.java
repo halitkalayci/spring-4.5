9a6d1219607f625b4e79757036b8b204e0aacc3e
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-09-07 15:39:12 +0300

    .

--- src/main/java/com/turkcell/intro/web/controller/ProductsController.java
+++ src/main/java/com/turkcell/intro/web/controller/ProductsController.java
@@ -2,13 +2,15 @@ package com.turkcell.intro.web.controller;
 
 import com.turkcell.intro.web.dto.product.request.CreateProductRequest;
 import com.turkcell.intro.web.dto.product.response.CreatedProductResponse;
+import com.turkcell.intro.web.dto.product.response.GetByIdProductResponse;
 import com.turkcell.intro.web.entity.Product;
 import com.turkcell.intro.web.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// Sayfalama-Filtreme-Özel Queryler
+// CCC
 @RestController // Spring tarafından RestController olarak tanınsın.
 @RequestMapping("/api/v1/products") // localhost:port/api/v1/products ile başlıyorsa istek buraya gelsin.
 public class ProductsController
@@ -26,4 +28,18 @@ public class ProductsController
         return productService.add(request);
     }
 
+    @GetMapping("{id}")
+    public GetByIdProductResponse getById(@PathVariable int id)
+    {
+        return productService.getById(id);
+    }
+
+    // Listeleme
+    // ID'e göre getirme
+    // Silme
+    // Ekleme
+    // Güncelleme
+    // CRUD
+
+    // Main entityler için mimari yapıya dikkat ederek bu 5 crud operasyonunu yapabilecek kodları yazalım.
 }
