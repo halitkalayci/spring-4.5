da52105743b02ebe613dd0522c4896339d7531d6
halitklyc <halitenes.kalayci@gmail.com>
2025-09-14 11:39:36 +0300

    mapper impl

--- src/main/java/com/turkcell/intro/web/mapper/ProductMapper.java
+++ src/main/java/com/turkcell/intro/web/mapper/ProductMapper.java
@@ -1,8 +1,16 @@
 package com.turkcell.intro.web.mapper;
 
+import com.turkcell.intro.web.dto.product.request.CreateProductRequest;
+import com.turkcell.intro.web.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ProductMapper
 {
+    // Dışarıdan bu mapper'a erişim için
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    Product createProductRequestToProduct(CreateProductRequest createProductRequest);
 }
